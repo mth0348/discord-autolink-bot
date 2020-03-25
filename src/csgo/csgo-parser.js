57f373b7aea0a62496553ed30aaa0fe92cb3b64f
Matthias Thomann <matthias.thomann@hotmail.com>
2020-03-25 14:54:40 +0100

    Fixed some stuff and added more emoji response support. Also removed item nesting when searching.

--- src/csgo/csgo-parser.js
+++ src/csgo/csgo-parser.js
@@ -8,19 +8,22 @@ const csgoList = require('./../data/csgo.json');
 class CsgoNadeParser {
     constructor(client) {
         this.mapOptions = { keys: ['map'] };
-        this.typeOptions = { keys: ['item.type'] };
-        this.sideOptions = { keys: ['item.side'] };
-        this.locationOptions = { keys: ['item.location'] };
+        this.typeOptions = { keys: ['type'] };
+        this.sideOptions = { keys: ['side'], threshold: 0 };
+        this.locationOptions = { keys: ['location'] };
     }
 
     startWorkflow(message) {
         if (message.content.length <= 7) {
-            return this.getHelpResponse();
+            this.embedResponse(message, this.getHelpResponse());
+            return null;
         }
 
         this.smoke_emoji = message.guild.emojis.cache.find(e => e.name === 'csgo_smoke');
         this.molotov_emoji = message.guild.emojis.cache.find(e => e.name === 'csgo_molotov_ct');
         this.flash_emoji = message.guild.emojis.cache.find(e => e.name === 'csgo_flash');
+        this.ct_emoji = message.guild.emojis.cache.find(e => e.name === 'csgo_ct');
+        this.t_emoji = message.guild.emojis.cache.find(e => e.name === 'csgo_t');
 
         let searchTerms = message.content.substring(7).split(" ");
         let results = csgoList;
@@ -34,7 +37,7 @@ class CsgoNadeParser {
             message.reply("Sorry, doesn't look like anything to me.");
         }
         else if (results.length === 1) {
-            let first = results[0].item;
+            let first = results[0];
             this.embedResponse(message, this.createItem(first));
         }
         else {
@@ -60,6 +63,36 @@ class CsgoNadeParser {
                                 return this.startWorkflow(message);
                         }
                     }).catch((e) => { console.log(e); });
+                }).catch((e) => { console.log(e); });
+            }
+
+            if (searchTerms.length === 2) {
+                message.channel.send(`There are ${results.length} clips for that. For which side do you want to search? Click the appropriate emoji.`).then(m => {
+                    m.react(this.t_emoji.id);
+                    m.react(this.ct_emoji.id);
+
+                    const filter = (reaction, user) => { return user.id !== m.author.id; };
+                    m.awaitReactions(filter, { max: 1, time: 20000, errors: ['time']} )
+                    .then(collected => {
+                        const reaction = collected.first();
+                        switch (reaction.emoji.id) {
+                            case this.t_emoji.id: 
+                                message.content += ' t';
+                                return this.startWorkflow(message);
+                            case this.ct_emoji.id: 
+                                message.content += ' ct';
+                                return this.startWorkflow(message);
+                        }
+                    }).catch((e) => { console.log(e); });
+                }).catch((e) => { console.log(e); });
+            }
+
+            if (searchTerms.length === 3) {
+                message.channel.send(`There are ${results.length} clips for that. Enter one of the following:.`).then(m => {
+                    for (let i = 0; i < results.length; i++) {
+                        const r = results[i];
+                        message.channel.send(`!nades ${r.map} ${r.type} ${r.side} ${r.location}`);
+                    }
                 }).catch((e) => { console.log(e); });
             }
         }
@@ -89,18 +122,21 @@ class CsgoNadeParser {
         message.channel.send(embed);
     }
 
-    populateResultWithSearch(results, searchTerms, index, searchOptions) {
+    populateResultWithSearch(results, searchTerms, index, searchOptions, isStrict) {
         if (searchTerms.length > index && results.length > 1) {
             let searchTerm = searchTerms[index];
             let fuse = new Fuse(results, searchOptions);
-            results = fuse.search(searchTerm);
+            let searchResults = fuse.search(searchTerm);
+            let newResults = [];
+            searchResults.forEach(e => {
+                newResults.push(e.item); /* this removes the unnecessary .item property nesting */
+            });
+            results = newResults;
         }
         return results;
     }
 
     createItem(data) {
-        if (data.item)
-            data = data.item;
         return new CsgoResponse(data.description, data.map, data.side, data.type, data.location, data.source);
     }
 
