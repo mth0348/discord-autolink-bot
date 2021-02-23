737d7f7ce71f2bc10665feea99e02eab19436e52
Matthias Thomann <matthias.thomann@hotmail.com>
2021-02-23 20:12:26 +0100

    Removed token.

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
 var drunkenbot_1 = require("./base/drunkenbot");
-var bot = new drunkenbot_1.DrunkenBot("NjkyMDM0MzkxNzM1NTMzNjI5.XnopQA.h0rK2LyIXSj-Wflwasa6ryE6LD8");
+var bot = new drunkenbot_1.DrunkenBot(process.env.TOKEN);
 bot.startListening();
 //# sourceMappingURL=index.js.map
