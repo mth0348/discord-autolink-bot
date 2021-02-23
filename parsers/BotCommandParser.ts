f6cae4e8224177f2440cb8e1df043233ce2462dc
Matthias Thomann <matthias.thomann@hotmail.com>
2021-02-23 19:44:29 +0100

    CSGO template.

--- parsers/BotCommandParser.ts
+++ parsers/BotCommandParser.ts
@@ -30,7 +30,7 @@ export class BotCommandParser extends BaseCommandParser {
         embed.setTitle("Bot Overview")
             .setDescription("This is the new DrunKen Discord Bot! Here are the registered modules:")
             .addField(`MtG Card Generator`, "Outputs randomly generated Magic The Gathering cards.\r\nCommands: `!mtg`, `!magic` or `!card`. Add `help` for more details.")
-            .addField(`CS GO Nades`, "Shows clips of how to precisely throw grenades in CS GO.\r\nCommands: `!nades`, `!cs` or `!csgo`. Add `help` for more details.")
+            // .addField(`CS GO Nades`, "Shows clips of how to precisely throw grenades in CS GO.\r\nCommands: `!nades`, `!cs` or `!csgo`. Add `help` for more details.")
             .addField(`(Coming soon)`, "More modules...")
             .setTimestamp()
             .setFooter("DrunKen Discord Bot", 'https://cdn.discordapp.com/icons/606196123660714004/da16907d73858c8b226486839676e1ac.png?size=128')
