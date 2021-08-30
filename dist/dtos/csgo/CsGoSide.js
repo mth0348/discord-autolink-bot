65edc1bbb9013cd8f055a113628b057f92ff7aa9
Matthias Thomann <matthias.thomann@hotmail.com>
2021-08-30 14:58:21 +0200

    remove dist from repo.

--- dist/dtos/csgo/CsGoSide.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.CsGoSide = void 0;
-var CsGoSide;
-(function (CsGoSide) {
-    CsGoSide["T"] = "t";
-    CsGoSide["CT"] = "ct";
-})(CsGoSide = exports.CsGoSide || (exports.CsGoSide = {}));
-//# sourceMappingURL=CsGoSide.js.map
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var drunkenbot_1 = require("./base/drunkenbot");
-var bot = new drunkenbot_1.DrunkenBot(process.env.TOKEN);
-bot.startListening();
-//# sourceMappingURL=index.js.map
